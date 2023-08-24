685ea8736753079011caf66e6cf660ddaeca3162
ajrlewis <git@ajrlewis.com>
2023-08-24 20:08:16 +0100

    Added ScalingParams to DataFrameProcessor class. Added extract method to extract columns from a data frame.

--- neurokit/utils/data_frame_processor.py
+++ neurokit/utils/data_frame_processor.py
@@ -4,14 +4,19 @@ import numpy as np
 
 
 class DataFrameProcessor:
+
+    ScalingParams = Dict[str, Dict[str, float]]  # e.g. {"foo": {"min": 0.0, "max": 1.0}, ...}
+
     @staticmethod
     def add_temporal_columns(data: pd.DataFrame, column: str = "date") -> pd.DataFrame:
+        week_scaling_param = 0.03773584905660377 * np.pi
+        weekday_scaling_param = 0.2857142857142857 * np.pi
         isocalendar = pd.to_datetime(data[column]).dt.isocalendar()
         data["year"] = isocalendar.year
-        data["week_sin"] = np.sin(2 * np.pi * isocalendar.week / 53)
-        data["week_cos"] = np.cos(2 * np.pi * isocalendar.week / 53)
-        data["weekday_sin"] = np.sin(2 * np.pi * isocalendar.day / 7)
-        data["weekday_cos"] = np.cos(2 * np.pi * isocalendar.day / 7)
+        data["week_sin"] = np.sin(week_scaling_param * isocalendar.week)
+        data["week_cos"] = np.cos(week_scaling_param * isocalendar.week)
+        data["weekday_sin"] = np.sin(weekday_scaling_param * isocalendar.day)
+        data["weekday_cos"] = np.cos(weekday_scaling_param * isocalendar.day)
         data = data.drop(column, axis=1)
         return data
 
@@ -24,62 +29,35 @@ class DataFrameProcessor:
     def scale_columns(
         data: pd.DataFrame,
         columns: List[str],
-        scaling_params: Optional[Dict[str, float]] = None,
-    ) -> Tuple[pd.DataFrame, Dict[str, float]]:
-        # Check if scaling_params is provided or not
+        scaling_params: Optional[ScalingParams] = None,
+    ) -> Tuple[pd.DataFrame, ScalingParams]:
         if scaling_params is None:
             scaling_params = {}
-
-            # Calculate min and max values for each column
             for col in columns:
                 min_value = data[col].min()
                 max_value = data[col].max()
-
-                # Store the scaling parameters in the scaling_params dictionary
                 scaling_params[col] = {"min": min_value, "max": max_value}
         else:
-            # Check if scaling_params contains the required columns
             for col in columns:
                 if col not in scaling_params:
                     raise ValueError(
                         f"scaling_params does not contain scaling parameters for column '{col}'"
                     )
-
-        # Scale the columns
         for col in columns:
             min_value = scaling_params[col]["min"]
             max_value = scaling_params[col]["max"]
-
-            # Apply the scaling transformation
             data[col] = (data[col] - min_value) / (max_value - min_value)
-
         return data, scaling_params
 
     @staticmethod
+    def extract(data: pd.DataFrame, columns: list) -> pd.DataFrame:
+        return data.filter(regex="|".join([column for column in columns]))
+
+    @staticmethod
     def split_data_by_date(data: pd.DataFrame, training_dataset_size: float = 0.8):
-        """
-        Split the data DataFrame into training and test samples based on the date column.
-
-        Parameters:
-        - data (pd.DataFrame): The input DataFrame to be split.
-        - training_dataset_size (float): The percentage of data to be used for training.
-
-        Returns:
-        - train_data (pd.DataFrame): The training dataset.
-        - test_data (pd.DataFrame): The test dataset.
-        """
-
-        # Get the unique dates in the dataset
         unique_dates = data["date"].unique()
-
-        # Calculate the index to split the data based on the training dataset size
         split_index = int(len(unique_dates) * training_dataset_size)
-
-        # Get the date at the split index
         split_date = unique_dates[split_index]
-
-        # Split the data into training and test sets based on the split date
         train_data = data[data["date"] < split_date]
         test_data = data[data["date"] >= split_date]
-
         return train_data, test_data
