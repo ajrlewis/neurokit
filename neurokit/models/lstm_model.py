dad88d07d50a3e195adc9d83a464b19205b6dd84
ajrlewis <git@ajrlewis.com>
2023-09-01 12:06:48 +0100

    Added type definitions to method arguments and return values. Change loss function to MAE in create model method. Adding IO methods to save the model. Added EarlyStopping and shuffled training data in training method.

--- neurokit/models/lstm_model.py
+++ neurokit/models/lstm_model.py
@@ -1,49 +1,69 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from keras.models import Sequential
-from keras.layers import LSTM, Dense
-from keras.callbacks import ModelCheckpoint
+from keras.callbacks import EarlyStopping
+from keras.layers import LSTM, Dense, Dropout
+from keras.models import load_model, Sequential
+from neurokit.utils.data_frame_processor import DataFrameProcessor
+
+
+def reshape(data: np.array) -> np.array:
+    return data.reshape(data.shape[0], data.shape[1], 1)
 
 
 class LSTMModel:
-    def __init__(self, features, target):
+    def __init__(self, features: np.array, target: np.array):
         self.features = features
         self.target = target
         self.model = None
 
     def create_model(self):
         self.model = Sequential()
-        self.model.add(LSTM(units=64, input_shape=(1, self.features.shape[1])))
+        self.model.add(
+            LSTM(
+                units=64,
+                input_shape=(self.features.shape[1], 1),
+                return_sequences=False,
+            )
+        )
         self.model.add(Dense(units=32, activation="relu"))
         self.model.add(Dense(units=1))
-        self.model.compile(optimizer="adam", loss="mean_squared_error")
+        self.model.compile(optimizer="adam", loss="mean_absolute_error")
 
-    def train_model(self, epochs=10, batch_size=32):
+    def train_model(self, epochs: int = 50, batch_size: int = 128):
         X_train, X_val, y_train, y_val = train_test_split(
-            self.features, self.target, test_size=0.2, shuffle=False
+            self.features, self.target, test_size=0.2, shuffle=True
         )
-        X_train_reshaped = np.reshape(X_train, (X_train.shape[0], 1, X_train.shape[1]))
-        X_val_reshaped = np.reshape(X_val, (X_val.shape[0], 1, X_val.shape[1]))
-        checkpoint = ModelCheckpoint(
-            "best_model.h5", save_best_only=True, save_weights_only=False
+        X_train_reshaped = reshape(X_train)
+        X_val_reshaped = reshape(X_val)
+        early_stopping = EarlyStopping(
+            monitor="val_loss", patience=10, restore_best_weights=True
         )
         self.model.fit(
-            X_train_reshaped,
+            X_train,
             y_train,
             epochs=epochs,
             batch_size=batch_size,
-            validation_data=(X_val_reshaped, y_val),
-            callbacks=[checkpoint],
+            validation_data=(X_val, y_val),
+            callbacks=[early_stopping],
         )
 
-    def save_model(self, filename="trained_model.h5"):
+    def forecast(self, data: np.array) -> np.array:
+        data_reshaped = reshape(data)
+        predictions = self.model.predict(data_reshaped)
+        return predictions.flatten()
+
+    def save_model(self, filename: str = "model.keras"):
         self.model.save(filename)
 
-    def forecast(self, data):
-        data_reshaped = np.reshape(data, (data.shape[0], 1, data.shape[1]))
-        predictions = self.model.predict(data_reshaped)
-        return predictions
+    def load_model(self, filename: str = "model.keras"):
+        self.model = load_model(filename)
+
+    def model_architecture(self):
+        return self.model.to_json()
+
+    def model_weights(self):
+        return self.model.get_weights()
 
 
 def main():
